fb1dd2131d35fc832e78d231579d5662fc80af4b
kavishashah653 <kavisha.qb@gmail.com>
2025-08-20 15:40:45 +0530

    Summarizer

---
+++ Summarizer/summarizer2.py
@@ -0,0 +1,17 @@
+from transformers import pipeline
+import pdfplumber 
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+def read_pdf(file_path):
+    text = ""
+    with pdfplumber.open(file_path) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text() 
+    return text
+
+file_path = r'C:\Users\ADMIN\Documents\GitHub\Transformers\Summarizer\docs\Photosynthesis.pdf'   
+pdf = read_pdf(file_path)
+
+summary = summarizer(pdf, max_length=200, min_length=100, do_sample=False)
+
+print(summary)
